69b9463cb6ea4a93b53f8e6c31f1a55338b62dec
Fina Ismatus Saniyah <89373176+FinaIsma@users.noreply.github.com>
2025-10-12 23:40:14 +0700

    JOBSHEET 7

---
+++ SMT 3/PEMWEB/JOBSHEET7/empty.php
@@ -0,0 +1,12 @@
+<?php
+$myArray = array();
+if (empty($myArray)){
+    echo "Array tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Array terdefinisi dan tidak kosong.<br>";
+}
+if (empty($nonExistentVar)){
+    echo "Variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Variabel terdefinii dan tidak kosong.<br>";
+}
